c64164e8c7ae3248dcb556478f165315f7d17a80
Thomas Ip <thomas@ipthomas.com>
2020-08-04 20:29:03 +0800

    Refactor calculator input

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -2,7 +2,7 @@
   <Application title="Calculator" :width="350" :height="450">
     <main>
       <div class="display-container">
-        <div class="display-text">{{ resultDisplay }}</div>
+        <div class="display-text">{{ display }}</div>
       </div>
       <div class="buttons-container">
         <button>%</button>
@@ -14,29 +14,29 @@
           <sub>𝑥</sub>
         </button>
 
-        <button @click="clearEntryPressed">CE</button>
-        <button @click="clearPressed">C</button>
-        <button @click="deletePressed"><b-icon-backspace /></button>
+        <button @click="clearEntry">CE</button>
+        <button @click="clear">C</button>
+        <button @click="deleteDigit"><b-icon-backspace /></button>
         <button class="bit-bigger">÷</button>
 
-        <button @click="digitPressed(7)" class="number">7</button>
-        <button @click="digitPressed(8)" class="number">8</button>
-        <button @click="digitPressed(9)" class="number">9</button>
+        <button @click="appendDigit(7)" class="number">7</button>
+        <button @click="appendDigit(8)" class="number">8</button>
+        <button @click="appendDigit(9)" class="number">9</button>
         <button class="bit-bigger">×</button>
 
-        <button @click="digitPressed(4)" class="number">4</button>
-        <button @click="digitPressed(5)" class="number">5</button>
-        <button @click="digitPressed(6)" class="number">6</button>
+        <button @click="appendDigit(4)" class="number">4</button>
+        <button @click="appendDigit(5)" class="number">5</button>
+        <button @click="appendDigit(6)" class="number">6</button>
         <button class="bit-bigger">−</button>
 
-        <button @click="digitPressed(1)" class="number">1</button>
-        <button @click="digitPressed(2)" class="number">2</button>
-        <button @click="digitPressed(3)" class="number">3</button>
+        <button @click="appendDigit(1)" class="number">1</button>
+        <button @click="appendDigit(2)" class="number">2</button>
+        <button @click="appendDigit(3)" class="number">3</button>
         <button class="bit-bigger">+</button>
 
         <button class="bit-bigger">±</button>
-        <button @click="digitPressed(0)" class="number">0</button>
-        <button @click="dotPressed" class="bit-bigger">.</button>
+        <button @click="appendDigit(0)" class="number">0</button>
+        <button @click="enterFractional" class="bit-bigger">.</button>
         <button class="bit-bigger">=</button>
       </div>
     </main>
@@ -44,78 +44,137 @@
 </template>
 
 <script lang='ts'>
-import { defineComponent, ref, computed } from 'vue';
+import { defineComponent, ref, computed, onMounted, reactive } from 'vue';
 import Application from './Application.vue';
 import BIconBackspace from './bootstrap_icons/BIconBackspace.vue';
 
 type digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+enum BinOp {
+  Add,
+  Sub,
+  Mul,
+  Div
+}
+
+interface WorkingNumber {
+  integer: number;
+  fractional: boolean | digit[];
+}
+
+function parseWorkingNumber(wn: WorkingNumber): number {
+  const fractional = Array.isArray(wn.fractional) && wn.fractional.length > 0 ?
+    Number.parseFloat('.' + wn.fractional.join('')) : 0;
+  return wn.integer + fractional;
+}
+
+function numberPretty(x: number): string {
+  return x.toLocaleString('en', { useGrouping: true });
+}
 
 export default defineComponent({
   name: 'Calculator',
   components: { Application, BIconBackspace },
   setup() {
-    const resultIsComputed = ref(false);
-    const result = ref(0);
-    const workingInteger = ref(0);
-    const workingFractional = ref<digit[]>([]);
-    const resultDisplay = computed(() => {
-      if (resultIsComputed.value) {
-        return result.value.toLocaleString('en', { useGrouping: true });
-      } else {
-        // Could have dangling 0s
-        let out = workingInteger.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        if (workingFractional.value.length > 0) {
-          out += '.' + workingFractional.value.join('');
-        }
-        return out;
-      }
+    const workingNumber = ref<WorkingNumber>({
+      integer: 0,
+      fractional: false,
     });
 
+    const display = computed(() => {
+      // Could have dangling 0s
+      let out = numberPretty(workingNumber.value.integer);
+      if (workingNumber.value.fractional != false) {
+        out += '.';
+      }
+      if (Array.isArray(workingNumber.value.fractional)) {
+        out += workingNumber.value.fractional.join('');
+      }
+      return out;
+    });
+
+    /* Handle input */
+
     let enteringFractional = false;
-    const digitPressed = (digit: digit) => {
-      if (!resultIsComputed.value) {
-        if (enteringFractional) {
-          workingFractional.value.push(digit);
-        } else {
-          workingInteger.value = workingInteger.value * 10 + digit;
-        }
+    const appendDigit = (digit: digit) => {
+      if (workingNumber.value.fractional === true) {
+        workingNumber.value.fractional = [];
       }
-    };
 
-    const dotPressed = () => {
-      if (!resultIsComputed.value && workingFractional.value.length === 0) {
-        enteringFractional = !enteringFractional;
-      }
-    };
-
-    const deletePressed = () => {
-      if (workingFractional.value.length > 0) {
-        workingFractional.value.pop();
-        if (workingFractional.value.length === 0) {
-          enteringFractional = false;
-        }
+      if (workingNumber.value.fractional) {
+        workingNumber.value.fractional.push(digit);
       } else {
-        workingInteger.value = Math.trunc(workingInteger.value / 10);
+        workingNumber.value.integer = workingNumber.value.integer * 10 + digit;
       }
     };
 
-    const clearEntryPressed = () => {
-      result.value = 0;
-      enteringFractional = false;
+    const enterFractional = () => {
+      if (typeof workingNumber.value.fractional === "boolean") {
+        workingNumber.value.fractional = !workingNumber.value.fractional;
+      }
     };
 
-    const clearPressed = () => {
-      result.value = 0;
-      enteringFractional = false;
-    }
+    const deleteDigit = () => {
+      if (Array.isArray(workingNumber.value.fractional)) {
+        workingNumber.value.fractional.pop();
+        if (workingNumber.value.fractional.length === 0) {
+          workingNumber.value.fractional = true;
+        }
+      } else if (workingNumber.value.fractional === true) {
+        workingNumber.value.fractional = false;
+      } else {
+        workingNumber.value.integer = Math.trunc(workingNumber.value.integer / 10);
+      }
+    };
+
+    const clearEntry = () => {
+      workingNumber.value = {
+        integer: 0,
+        fractional: false,
+      };
+    };
+
+    const clear = () => {
+      clearEntry();
+    };
+
+    onMounted(() => {
+      window.addEventListener('keydown', (event: KeyboardEvent) => {
+        switch (event.key) {
+          case '0':
+          case '1':
+          case '2':
+          case '3':
+          case '4':
+          case '5':
+          case '6':
+          case '7':
+          case '8':
+          case '9':
+            appendDigit(Number.parseInt(event.key) as digit);
+            break;
+          case '.':
+            enterFractional();
+            break;
+          case 'Backspace':
+            deleteDigit();
+            break;
+          case 'Delete':
+            clearEntry();
+            break;
+          case 'Escape':
+            clear();
+            break;
+        }
+      });
+    });
 
     return {
-      resultDisplay,
-      digitPressed,
-      dotPressed,
-      deletePressed,
-      clearEntryPressed,
-      clearPressed,
+      display,
+      appendDigit,
+      enterFractional,
+      deleteDigit,
+      clearEntry,
+      clear,
     };
   },
 });
